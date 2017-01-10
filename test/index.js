9d0d778e2f13efc1590f6193df7fc8a3539f247e
Jannis R <mail@jannisr.de>
2017-01-10 02:07:31 +0100

    basic Selenium test

---
+++ test/index.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const webdriver = require('webdriverio')
+const so = require('so')
+const assert = require('assert')
+
+const user = process.env.SAUCE_USERNAME
+const key = process.env.SAUCE_ACCESS_KEY
+const build = process.env.TRAVIS_BUILD_NUMBER
+const job = process.env.TRAVIS_JOB_NUMBER
+const browser = process.env.BROWSER
+const platform = process.env.PLATFORM
+
+console.log(`Using ${browser} on ${platform}.`)
+
+
+
+const runner = webdriver.remote({
+	user, key, host: 'localhost', port: 4445,
+	logLevel: 'silent',
+	desiredCapabilities: {
+		build, 'tunnel-identifier': job, name: 'test',
+		browserName: browser, platform,
+		recordScreenshots: false
+	}
+})
+
+so(function* () {
+	yield runner.init()
+	yield runner.url(`http://localhost:8080/index.html`)
+
+	const json = yield runner.execute(() => document.getElementById('out').innerText)
+	console.info(json)
+	assert.doesNotThrow(() => JSON.parse(json))
+	const data = JSON.parse(json)
+
+	assert.strictEqual(typeof data.latitude, 'number')
+	assert.strictEqual(typeof data.longitude, 'number')
+	assert.strictEqual(typeof data.precision, 'number')
+	assert.strictEqual(typeof data.native, 'boolean')
+
+	console.log('Done.')
+	yield runner.end()
+})()
+.catch((err) => {
+	console.error(err)
+	process.exit(1)
+})
